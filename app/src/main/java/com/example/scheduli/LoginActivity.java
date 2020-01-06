2df569d45feb4c73c24627ad525e26cd56ddef0c
Daniel Szuster <37206928+SzustyDaniel@users.noreply.github.com>
2020-01-06 14:34:30 +0200

    added app icon. started work on login implementation

--- app/src/main/java/com/example/scheduli/LoginActivity.java
+++ app/src/main/java/com/example/scheduli/LoginActivity.java
@@ -1,14 +1,36 @@
 package com.example.scheduli;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.scheduli.Utils.UsersUtils;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText userEmail, userPassword;
+    private Button createAccount, loginToApp, forgotPassword;
+    private UsersUtils usersUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usersUtils = UsersUtils.getInstance();
+        initView();
+
+
     }
+
+    private void initView() {
+        userEmail = findViewById(R.id.et_login_email);
+        userPassword = findViewById(R.id.et_login_password);
+        createAccount = findViewById(R.id.btn_login_signup);
+        loginToApp = findViewById(R.id.btn_login_to_account);
+        forgotPassword = findViewById(R.id.btn_login_forgot_password);
+    }
+
+
 }
