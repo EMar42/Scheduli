63fed29cf385ef14ea06cd85956f86a15ad2eec2
Daniel Szuster <danielszu23@gmail.com>
2020-01-14 10:50:15 +0200

    added user information pull from database

--- app/src/main/java/com/example/scheduli/data/UserDataRepository.java
+++ app/src/main/java/com/example/scheduli/data/UserDataRepository.java
@@ -2,12 +2,16 @@ package com.example.scheduli.data;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 
 import com.example.scheduli.data.fireBase.FirebaseQueryLiveData;
+import com.example.scheduli.data.fireBase.UserDataCallBack;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -56,6 +60,23 @@ public class UserDataRepository {
         dataBaseReference.child(uid).child("appointments").setValue(appointments);
     }
 
+    public void getUserFromUid(final String uid, final UserDataCallBack callBack) {
+        ValueEventListener userEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.i(TAG_USER_REPOSITORY, "Getting data from database on " + uid);
+                User user = dataSnapshot.child(uid).getValue(User.class);
+                callBack.onCallback(user);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG_USER_REPOSITORY, databaseError.getMessage());
+            }
+        };
+
+        this.dataBaseReference.addListenerForSingleValueEvent(userEventListener);
+    }
 
 
 }
---
+++ app/src/main/java/com/example/scheduli/data/fireBase/UserDataCallBack.java
@@ -0,0 +1,7 @@
+package com.example.scheduli.data.fireBase;
+
+import com.example.scheduli.data.User;
+
+public interface UserDataCallBack {
+    public void onCallback(User user);
+}
